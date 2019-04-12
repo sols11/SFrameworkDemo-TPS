1a5106b0d0d1c08c87efd75262e322427cf1f7d4
sols11 <caodahan97@126.com>
2019-04-12 14:04:37 +0800

    完成了子弹壳特效

--- Assets/Scripts/ProjectScript/Weapon/WeaponHandgun.cs
+++ Assets/Scripts/ProjectScript/Weapon/WeaponHandgun.cs
@@ -8,8 +8,12 @@ namespace ProjectScript
     public class WeaponHandgun : IPlayerWeapon
     {
         public Transform bulletPosition;
+        public Transform shellPosition;
         public GameObject muzzleFlashEffect;
         private string shootEffect = @"Particles\BulletTrail";
+        private string shellEffect = @"Particles\EmptyShell";
+        private Vector3 emptyShellMinForce = new Vector3(0.8f, -0.5f, 0.2f);
+        private Vector3 emptyShellMaxForce = new Vector3(-1.2f, 0.8f, 0.4f);
 
         public override void Attack()
         {
@@ -22,6 +26,10 @@ namespace ProjectScript
             // 特效
             muzzleFlashEffect.SetActive(true);
             resourcesMgr.LoadAsset(shootEffect, true, bulletPosition.position, Quaternion.identity);
+            // 子弹壳
+            Vector3 randomEmptyShellForce = new Vector3(Random.Range(emptyShellMinForce.x,  emptyShellMaxForce.x), Random.Range( emptyShellMinForce.y,  emptyShellMaxForce.y), Random.Range( emptyShellMinForce.z,  emptyShellMaxForce.z));
+            GameObject emptyShell = resourcesMgr.LoadAsset(shellEffect, true, shellPosition.position, shellPosition.rotation);
+            emptyShell.GetComponent<Rigidbody>().AddForce(shellPosition.rotation * randomEmptyShellForce, ForceMode.Impulse);
 
             // 碰撞检测
             Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
