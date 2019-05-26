429cf87f1a1d4357740a9352b3dbe5de2086eb27
sols11 <caodahan97@126.com>
2019-05-26 12:12:53 +0800

    添加血包和弹夹模型

---
+++ Assets/Scripts/SFramework/MonoFunction/AutoRotate.cs
@@ -0,0 +1,33 @@
+﻿/*----------------------------------------------------------------------------
+Author:
+    Anotts
+Date:
+    2017/08/01
+Description:
+    简介：
+    作用：自动旋转
+    使用：
+    补充：
+History:
+----------------------------------------------------------------------------*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace SFramework
+{
+    /// <summary>
+    /// 自动旋转
+    /// </summary>
+	public class AutoRotate : MonoBehaviour
+    {
+        public Vector3 rotate;
+
+        private void Update()
+        {
+            transform.Rotate(rotate);
+        }
+    }
+}
